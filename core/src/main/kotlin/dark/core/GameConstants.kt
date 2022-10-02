168ed29564be544762595f289cefee13ba614a6e
Tommie Nygren <tommie.nygren@gmail.com>
2022-10-02 14:24:10 +0200

    More bullshit setup stuff

--- core/src/main/kotlin/dark/core/GameConstants.kt
+++ core/src/main/kotlin/dark/core/GameConstants.kt
@@ -1,5 +1,8 @@
 package dark.core
 
 class GameSettings {
-    val GameWidth =
+    val GameWidth =42f
+    val GameHeight = (16f/9f) * GameWidth
+    val PixelsPerMeter = 8f
+    val MetersPerPixel = 1f / PixelsPerMeter
 }
--- core/src/main/kotlin/dark/injection/Context.kt
+++ core/src/main/kotlin/dark/injection/Context.kt
@@ -1,14 +1,19 @@
 package dark.injection
 
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.github.quillraven.fleks.World
 import com.github.quillraven.fleks.world
 import dark.core.GameSettings
 import eater.injection.InjectionContext
+import ktx.assets.DisposableContainer
+import ktx.assets.DisposableRegistry
 import ktx.assets.disposeSafely
+import ktx.assets.toInternalFile
 import ktx.box2d.createWorld
 
 object Context : InjectionContext() {
@@ -26,12 +31,36 @@ object Context : InjectionContext() {
             bindSingleton(GameSettings())
             bindSingleton(getFleksWorld())
             bindSingleton(createWorld())
+            bindSingleton(Assets())
         }
     }
 
     private fun getFleksWorld(): World {
         return world {
-            
+
         }
+    }
+}
+
+sealed class AnimDef(val name: String, val rowIndex: Int, val numberOfFrames: Int) {
+    object Walk: AnimDef("walk", 3, 4)
+    object Interact: AnimDef("interact", 1, 3)
+
+    companion object {
+        val animDefs = listOf(Walk, Interact)
+    }
+}
+
+class Assets: DisposableRegistry by DisposableContainer() {
+    val buddy: Map<AnimDef, Animation<TextureRegion>> by lazy {
+        val texture = Texture("player/buddy.png".toInternalFile()).alsoRegister()
+        AnimDef.animDefs.associateWith { ad ->
+            Animation(0.1f, *Array(ad.numberOfFrames) { x ->
+                TextureRegion(texture, x * 32, ad.rowIndex * 32, 32, 32)
+            })
+        }
+    }
+    override fun dispose() {
+        registeredDisposables.disposeSafely()
     }
 }
