2a554a76ce78efd13574abd28829c6f942a7d124
Tommie Nygren <tommie.nygren@gmail.com>
2022-10-10 12:52:00 +0200

    Formatted render code for readability

--- core/src/main/kotlin/dark/ecs/systems/RenderSystem.kt
+++ core/src/main/kotlin/dark/ecs/systems/RenderSystem.kt
@@ -41,14 +41,21 @@ class RenderSystem(
     private val mapFamily = allOf(Map::class).get()
     private val mapEntity get() = engine.getEntitiesFor(mapFamily).first() //Should always be one
     private val foodFamily = allOf(Food::class, Box2d::class).get()
-    private val fbo by lazy { FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.width * 2, Gdx.graphics.height * 2, true) }
+    private val fbo by lazy {
+        FrameBuffer(
+            Pixmap.Format.RGBA8888,
+            Gdx.graphics.width * 2,
+            Gdx.graphics.height * 2,
+            true
+        )
+    }
     private val blobCenter = vec3()
     private val shaderCenter = vec2()
     private val shockParams = vec3(10.0f, 0.2f, 0.1f)
     private var shaderTime = 2f
     override fun update(deltaTime: Float) {
         shaderTime - deltaTime
-        if(shaderTime < 0f)
+        if (shaderTime < 0f)
             shaderTime = 2f
 
         batch.projectionMatrix = camera.combined
@@ -58,7 +65,11 @@ class RenderSystem(
             Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT)
             //Render the map. i.e. draw its region
             val map = Map.get(mapEntity)
-            batch.draw(map.mapTextureRegion,map.mapOrigin.x - map.mapTextureRegion.regionWidth / 2f, map.mapOrigin.y - map.mapTextureRegion.regionHeight / 2f)
+            batch.draw(
+                map.mapTextureRegion,
+                map.mapOrigin.x - map.mapTextureRegion.regionWidth / 2f,
+                map.mapOrigin.y - map.mapTextureRegion.regionHeight / 2f
+            )
 
             for (blobList in BlobGrouper.blobGroups) {
                 for ((index, blobEntity) in blobList.withIndex()) {
@@ -69,7 +80,16 @@ class RenderSystem(
                     val blobPosition = blobBody.position
                     val blob = Blob.get(blobEntity)
                     val health = PropsAndStuff.get(blobEntity).getHealth()
-                    shapeDrawer.filledCircle(blobPosition, 1f, Color(1f-health.normalizedValue,health.normalizedValue, 1f-health.normalizedValue, health.normalizedValue))
+                    shapeDrawer.filledCircle(
+                        blobPosition,
+                        1f,
+                        Color(
+                            1f - health.normalizedValue,
+                            health.normalizedValue,
+                            1f - health.normalizedValue,
+                            health.normalizedValue
+                        )
+                    )
                     shapeDrawer.setColor(Color.GREEN)
 
                     if (CameraFollow.has(blobEntity))
@@ -81,7 +101,11 @@ class RenderSystem(
             val foodRenderStuff = engine.getEntitiesFor(foodFamily)
                 .associate { Box2d.get(it).body.position to MathUtils.norm(0f, 100f, Food.get(it).foodEnergy) }
             for ((position, normalizedEnergy) in foodRenderStuff) {
-                shapeDrawer.filledCircle(position, 2.5f * normalizedEnergy, Color(1f-normalizedEnergy, normalizedEnergy, 1f-normalizedEnergy, normalizedEnergy))
+                shapeDrawer.filledCircle(
+                    position,
+                    2.5f * normalizedEnergy,
+                    Color(1f - normalizedEnergy, normalizedEnergy, 1f - normalizedEnergy, normalizedEnergy)
+                )
             }
         }
         fbo.end()
@@ -102,7 +126,13 @@ class RenderSystem(
             // and.... FLIP!  V (vertical) only
             // and.... FLIP!  V (vertical) only
             textureRegion.flip(false, true)
-            batch.draw(textureRegion, camera.position.x - camera.viewportWidth / 2f, camera.position.y - camera.viewportHeight / 2f, camera.viewportWidth, camera.viewportHeight)
+            batch.draw(
+                textureRegion,
+                camera.position.x - camera.viewportWidth / 2f,
+                camera.position.y - camera.viewportHeight / 2f,
+                camera.viewportWidth,
+                camera.viewportHeight
+            )
             batch.shader = null
         }
     }
